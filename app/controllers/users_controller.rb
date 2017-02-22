3d269572f7768958c885682df0fca6cd6df98913
Ubuntu <ubuntu@rails-dev-box>
2017-02-22 13:28:49 +0000

    create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,25 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new user_params
+    if @user.save!
+      session[:user_id] = @user.id
+      flash[:success] = "User created successful!"
+      redirect_to user_path(id: @user.id)
+    else
+      raise @user.errors.full_messages.to_sentence
+      flash[:error] = "Error: #{@user.errors.full_messages.to_sentence}"
+      render 'new'
+    end
+  end
+
   def index
     @users = User.all.shuffle
   end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+
 end
