4cf1b42bffc16200d7f2b0f3a1bc1ca53d9be1f0
Ubuntu <ubuntu@rails-dev-box>
2017-02-22 13:48:30 +0000

    milestone 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+  get 'login' => 'sessions#new'
   root 'welcome#index'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
